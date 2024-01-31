66871f6463d7561ac00c4891fa85f9d9ecda649f
Raymon Roos <raymon.roos@hotmail.com>
2024-01-31 16:52:43 +0100

    fix(migrations): `heroes`.`user_id` should be unique

--- database/migrations/03_2024_01_29_141745_create_heroes_table.php
+++ database/migrations/03_2024_01_29_141745_create_heroes_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('heroes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')
+                ->unique()
                 ->constrained()
                 ->cascadeOnUpdate()
-                ->cascadeOnDelete();
+                ->restrictOnDelete();
             $table->string('hero_alias')
                 ->unique();
             $table->string('emergency_contact');
