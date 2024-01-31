3fc70b60b9f932dc2209db28e267535ea8c1c90b
Emile Muntinga <emilemuntinga@gmail.com>
2024-01-31 16:08:39 +0100

    fix: merge conflict fix

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -10,7 +10,7 @@
                  @if ($key == 0)
                     <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                 @else
-                    <div x-data="{open: false}" class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 @endif
                         <div class="absolute z-40 top-10 left-20">
                             <h1 class="text-4xl font-extrabold bg-red-800 ease-in-out duration-500 hover:scale-125 text-white px-2 py-4 rounded-bl-lg border-2 border-black rounded-tr-lg z-100 opacity-100 blur-none">Leaderboard</h1>
@@ -18,13 +18,18 @@
                         <div class="absolute z-40 top-10 right-20">
                             <h1 class="flex text-9xl font-extrabold bg-red-800 ease-in-out duration-500 hover:scale-125 text-white px-2 py-4 rounded-bl-lg border-2 border-black rounded-tr-lg z-100 opacity-100 blur-none"> #{{ $key + 1 }} </h1>
                         </div>
-                        <img src="{{ asset($hero['image_path']) }}" class="absolute z-10 w-[150vw] opacity-70 blur-2xl block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $hero['name'] }}">
-                        <img @mouseover="open = true" src="{{ asset($hero['image_path']) }}" class="absolute h-[80vh] hover:translate-x-2 ease-in-out duration-500 hover:scale-110 z-30 block shadow-xl object-contain rounded-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $hero['name'] }}">
-                        <div x-show="open" x-transition.scale.origin.10 class="absolute bg-opacity-15 mx-auto w-[60vw] h-[50vh] bg-red-800 border-2 border-black z-20 block -translate-x-3/4 -translate-y-2/4 top-1/2 left-1/2 rounded-bl-xl rounded-tr-lg">
+                        <img id="background-image" src="{{ asset($hero['image_path']) }}" class="absolute z-10 w-[150vw] opacity-70 blur-2xl block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $hero['name'] }}">
+                        <img id="image" src="{{ asset($hero['image_path']) }}" class="absolute h-[80vh] z-30 block shadow-xl object-contain rounded-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $hero['name'] }}">
+                        <div class="absolute bg-opacity-15 mx-auto w-[60vw] h-[50vh] bg-red-800 border-2 border-black z-20 block -translate-x-3/4 -translate-y-2/4 top-1/2 left-1/2 rounded-bl-xl rounded-tr-lg">
                             <h1 class="text-white text-6xl font-extrabold p-2"> {{ $hero['name'] }} </h1>
                         </div>
                     </div>
                 @endforeach
+            </div>
+            <div class="absolute  bg-red-800 text-white p-2 z-20 flex overflow-x-scroll rounded-bl-lg rounded-tr-lg border-2 border-black overflow-y-hidden -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2 blur-none">
+                @for ($i = 0; $i < count($heroes); $i++)
+                    <button type="button" class="flex !w-6 !h-8 px-6 py-3 !bg-red-800 ease-in-out duration-100 hover:scale-125 !text-white blur-none justify-center items-center opacity-100 z-20 text-center" aria-current="true" aria-label="Slide {{ $i }}" data-carousel-slide-to="{{ $i }}">{{ $i + 1 }}</button>
+                @endfor
             </div>
             <!-- Slider controls -->
             <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
@@ -45,4 +50,17 @@
             </button>
         </div>        
     </main>
+    <script>
+        const background = document.getElementById('background-image');
+        const image = document.getElementById('image');
+        
+        image.addEventListener('mouseover', function() {
+            background.classList.remove('blur-2xl');
+        });
+
+        image.addEventListener('mouseout', function() {
+            background.classList.add('blur-2xl');
+        });
+
+    </script>
 @endsection
