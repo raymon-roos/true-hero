399cce8ed65b40c0cf36e5ddc6aa9a3f0d2e1a69
Raymon Roos <raymon.roos@hotmail.com>
2024-01-31 16:59:09 +0100

    feat(migrations): add some default review statuses

--- database/migrations/01_2024_01_29_145651_create_review_statuses_table.php
+++ database/migrations/01_2024_01_29_145651_create_review_statuses_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ReviewStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,13 @@ return new class extends Migration
             $table->string('status');
             $table->timestamps();
         });
+
+        ReviewStatus::insertOrIgnore([
+            ['status' => 'pending'],
+            ['status' => 'accepted'],
+            ['status' => 'rejected'],
+            ['status' => 'requires verification'],
+        ]);
     }
 
     /**
