f1084ba044a48375bb6fc9351be83ad0fddea60a
Raymon Roos <raymon.roos@hotmail.com>
2024-01-31 23:08:04 +0100

    feat(seeders): seed some hard-coded heroes

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Enums\HeroRating;
 use App\Models\Hero;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
@@ -23,5 +22,51 @@ class HeroSeeder extends Seeder
         Hero::factory(count($userIds))
             ->sequence(fn (Sequence $sequence) => [ 'user_id' => $userIds[$sequence->index] ])
             ->create();
+
+        Hero::factory()
+            ->sequence([
+                    [
+                        'hero_alias' => 'Pyroclast',
+                        'superpower' => 'Control over fire',
+                        'elo_rating' => 1850,
+                        'image_path' => '/img/pyroclast.png'
+                    ],
+                    [
+                        'hero_alias' => 'Aeromaster',
+                        'superpower' => 'Manipulate wind',
+                        'elo_rating' => 1780,
+                        'image_path' => '/img/aeromaster.png'
+                    ],
+                    [
+                        'hero_alias' => 'Titan Guard',
+                        'superpower' => 'Superhuman strength and durability',
+                        'elo_rating' => 1920,
+                        'image_path' => '/img/titan-guard.png'
+                    ],
+                    [
+                        'hero_alias' => 'Phantom Veil',
+                        'superpower' => 'Invisibility',
+                        'elo_rating' => 1700,
+                        'image_path' => '/img/phantom-veil.png'
+                    ],
+                    [
+                        'hero_alias' => 'Thunderstrike',
+                        'superpower' => 'Control over electricity',
+                        'elo_rating' => 1820,
+                        'image_path' => '/img/thunderstrike.png'
+                    ],
+                    [
+                        'hero_alias' => 'Mindwave',
+                        'superpower' => 'Telekinetic abilities',
+                        'elo_rating' => 1760,
+                        'image_path' => '/img/mindwave.png'
+                    ],
+                    [
+                        'hero_alias' => 'Blitz Runner',
+                        'superpower' => 'Super speed',
+                        'elo_rating' => 1890,
+                        'image_path' => '/img/blitzrunner.png'
+                    ],
+            ]);
     }
 }
