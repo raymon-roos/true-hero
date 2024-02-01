579200a4681d2de25d09bdbe2b57a625ba248d1e
Raymon Roos <raymon.roos@hotmail.com>
2024-02-01 09:18:46 +0100

    fix(seeders): fix bad `profile_picture` fields in `HeroSeeder`

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -14,59 +14,64 @@ class HeroSeeder extends Seeder
      */
     public function run(): void
     {
-        $userIds = User::where('is_admin', false)
-            ->inRandomOrder()
-            ->pluck('id')
-            ->all();
-
-        Hero::factory(count($userIds))
-            ->sequence(fn (Sequence $sequence) => [ 'user_id' => $userIds[$sequence->index] ])
-            ->create();
+        $userIds = User::pluck('id');
 
         Hero::factory()
-            ->sequence([
-                    [
-                        'hero_alias' => 'Pyroclast',
-                        'superpower' => 'Control over fire',
-                        'elo_rating' => 1850,
-                        'image_path' => '/img/pyroclast.png'
-                    ],
-                    [
-                        'hero_alias' => 'Aeromaster',
-                        'superpower' => 'Manipulate wind',
-                        'elo_rating' => 1780,
-                        'image_path' => '/img/aeromaster.png'
-                    ],
-                    [
-                        'hero_alias' => 'Titan Guard',
-                        'superpower' => 'Superhuman strength and durability',
-                        'elo_rating' => 1920,
-                        'image_path' => '/img/titan-guard.png'
-                    ],
-                    [
-                        'hero_alias' => 'Phantom Veil',
-                        'superpower' => 'Invisibility',
-                        'elo_rating' => 1700,
-                        'image_path' => '/img/phantom-veil.png'
-                    ],
-                    [
-                        'hero_alias' => 'Thunderstrike',
-                        'superpower' => 'Control over electricity',
-                        'elo_rating' => 1820,
-                        'image_path' => '/img/thunderstrike.png'
-                    ],
-                    [
-                        'hero_alias' => 'Mindwave',
-                        'superpower' => 'Telekinetic abilities',
-                        'elo_rating' => 1760,
-                        'image_path' => '/img/mindwave.png'
-                    ],
-                    [
-                        'hero_alias' => 'Blitz Runner',
-                        'superpower' => 'Super speed',
-                        'elo_rating' => 1890,
-                        'image_path' => '/img/blitzrunner.png'
-                    ],
-            ]);
+            ->sequence(
+                [
+                    'hero_alias' => 'Pyroclast',
+                    'superpower' => 'Control over fire',
+                    'elo_rating' => 1850,
+                    'profile_picture' => 'hero-profile-pictures/pyroclast.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Aeromaster',
+                    'superpower' => 'Manipulate wind',
+                    'elo_rating' => 1780,
+                    'profile_picture' => 'hero-profile-pictures/aeromaster.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Titan Guard',
+                    'superpower' => 'Superhuman strength and durability',
+                    'elo_rating' => 1920,
+                    'profile_picture' => 'hero-profile-pictures/titan-guard.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Phantom Veil',
+                    'superpower' => 'Invisibility',
+                    'elo_rating' => 1700,
+                    'profile_picture' => 'hero-profile-pictures/phantom-veil.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Thunderstrike',
+                    'superpower' => 'Control over electricity',
+                    'elo_rating' => 1820,
+                    'profile_picture' => 'hero-profile-pictures/thunderstrike.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Mindwave',
+                    'superpower' => 'Telekinetic abilities',
+                    'elo_rating' => 1760,
+                    'profile_picture' => 'hero-profile-pictures/mindwave.png',
+                    'user_id' => $userIds->pop(),
+                ],
+                [
+                    'hero_alias' => 'Blitz Runner',
+                    'superpower' => 'Super speed',
+                    'elo_rating' => 1890,
+                    'profile_picture' => 'hero-profile-pictures/blitzrunner.png',
+                    'user_id' => $userIds->pop(),
+                ],
+            )
+        ->create();
+
+        Hero::factory(count($userIds))
+            ->sequence(fn (Sequence $sequence) => [ 'user_id' => $userIds->pop() ])
+            ->create();
     }
 }
