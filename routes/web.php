01798dac6cc99735d22bae30973d0a48c69310df
NickProoij <prooijn@gmail.com>
2024-02-01 12:08:05 +0100

    feat(routes): add and organize registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,91 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Homepage and Static Pages
 Route::get('/', function () {
     return view('leaderboard', [
         'heroes' => [
-                [
-                    "name" => "Pyroclast",
-                    "superpower" => "Control over fire",
-                    "elo_score" => 1850,
-                    "image_path" => "/img/pyroclast.png"
-                ],
-                [
-                    "name" => "Aeromaster",
-                    "superpower" => "Manipulate wind",
-                    "elo_score" => 1780,
-                    "image_path" => "/img/aeromaster.png"
-                ],
-                [
-                    "name" => "Titan Guard",
-                    "superpower" => "Superhuman strength and durability",
-                    "elo_score" => 1920,
-                    "image_path" => "/img/titan-guard.png"
-                ],
-                [
-                    "name" => "Phantom Veil",
-                    "superpower" => "Invisibility",
-                    "elo_score" => 1700,
-                    "image_path" => "/img/phantom-veil.png"
-                ],
-                [
-                    "name" => "Thunderstrike",
-                    "superpower" => "Control over electricity",
-                    "elo_score" => 1820,
-                    "image_path" => "/img/thunderstrike.png"    
-                ],
-                [
-                    "name" => "Mindwave",
-                    "superpower" => "Telekinetic abilities",
-                    "elo_score" => 1760,
-                    "image_path" => "/img/mindwave.png"
-                ],
-                [
-                    "name" => "Blitz Runner",
-                    "superpower" => "Super speed",
-                    "elo_score" => 1890,
-                    "image_path" => "/img/blitzrunner.png"
-                ],
-                // [
-                //     "name" => "Beastmorph",
-                //     "superpower" => "Transform into any animal",
-                //     "elo_score" => 1800,
-                //     "image_path" => "/img/beastmorph.png"
-                // ],
-                // [
-                //     "name" => "Time Weaver",
-                //     "superpower" => "Manipulate time",
-                //     "elo_score" => 1950,
-                //     "image_path" => "/img/time-weaver.png"
-                // ],
-                // [
-                //     "name" => "Luminance",
-                //     "superpower" => "Light manipulation",
-                //     "elo_score" => 1880,
-                //     "image_path" => "/img/luminance.png"
-                // ]
-            ]
-        ]);
-    });
+            ["name" => "Pyroclast", "superpower" => "Control over fire", "elo_score" => 1850, "image_path" => "/img/pyroclast.png"],
+            ["name" => "Aeromaster", "superpower" => "Manipulate wind", "elo_score" => 1780, "image_path" => "/img/aeromaster.png"],
+            ["name" => "Titan Guard", "superpower" => "Superhuman strength and durability", "elo_score" => 1920, "image_path" => "/img/titan-guard.png"],
+            ["name" => "Phantom Veil", "superpower" => "Invisibility", "elo_score" => 1700, "image_path" => "/img/phantom-veil.png"],
+            ["name" => "Thunderstrike", "superpower" => "Control over electricity", "elo_score" => 1820, "image_path" => "/img/thunderstrike.png"],
+            ["name" => "Mindwave", "superpower" => "Telekinetic abilities", "elo_score" => 1760, "image_path" => "/img/mindwave.png"],
+            ["name" => "Blitz Runner", "superpower" => "Super speed", "elo_score" => 1890, "image_path" => "/img/blitzrunner.png"],
+        ]
+    ]);
+});
 
-    Route::get('index', function() {
-        return view('index');
-    });
+Route::get('index', function() { return view('index'); });
+Route::get('page1', function() { return view('page1'); });
+Route::get('page2', function() { return view('page2'); });
+Route::get('page3', function() { return view('page3'); });
+Route::get('success', function() { return view('success'); });
 
-    Route::get('page1', function() {
-        return view('page1');
-    });
-
-    Route::get('page2', function() {
-        return view('page2');
-    });
-
-    Route::get('page3', function() {
-        return view('page3');
-    });
-
-    Route::get('success', function() {
-        return view('success');
-    });
-
-    
+// Registration Routes
+Route::post('register/step1', [RegistrationController::class, 'storeStep1'])->name('register.step1');
+Route::post('register/step2', [RegistrationController::class, 'storeStep2'])->name('register.step2');
+Route::post('register/step3', [RegistrationController::class, 'storeStep3'])->name('register.step3');
