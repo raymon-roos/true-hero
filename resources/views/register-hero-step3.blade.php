9f57ff5d7c978db095543c06bbba73272bbe9377
NickProoij <prooijn@gmail.com>
2024-02-02 09:47:41 +0100

    fix(registration) previous button text color

--- resources/views/register-hero-step3.blade.php
+++ resources/views/register-hero-step3.blade.php
@@ -40,7 +40,7 @@
         </div>
 
         <button id="backbutton" type="button"
-            class="px-5 py-2.5 text-sm font-medium text-red-600 hover:text-gray-50 border-2 border-red-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+            class="px-5 py-2.5 text-sm font-medium text-gray-50 border-2 border-red-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
             < Previous</button>
 
                 <button type="submit"
